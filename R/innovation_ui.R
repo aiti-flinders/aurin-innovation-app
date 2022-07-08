c76d0d00574304bd6404df3e8379d7ea488313ac
Hamish Gamble <gamb0043@flinders.edu.au>
2022-07-08 16:39:49 +0930

    Switch to tabPanel

--- R/innovation_ui.R
+++ R/innovation_ui.R
@@ -1,12 +1,10 @@
 innovation_ui <- function() {
   navbarPage("Regional Innovation",
-             navbarMenu(title = "Regional Innovation",
-                        innovationMapUI("innovation_map", data = innovation_data),
-                        innovationCompareUI("innovation_compare", data = innovation_data)
+             tabPanel(title = "Regional Innovation",
+                        innovationMapUI("innovation_map", data = innovation_data)
              ),
-             navbarMenu("More",
-                        tabPanel("Something MORE",
-                                 includeHTML("https://aiti-flinders.github.io/aurin-innovation/articles/industrial_growth_opportunities.html"))
+             tabPanel(title = "Compare Regions",
+                        innovationCompareUI("innovation_compare", data = innovation_data)
              )
   )
 }
